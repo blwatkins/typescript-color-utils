1daf49a302a81bf7cc856187f491a88bbdf8ebb6
blwatkins <blwatkins@users.noreply.github.com>
2026-07-21 21:41:11 -0500

    Add initial tests for rgb-builder.test.ts

--- test/rgb/rgb-builder.test.ts
+++ test/rgb/rgb-builder.test.ts
@@ -17,3 +17,54 @@
  * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+import { describe, test, expect } from 'vitest';
+import { RGB, RGBBuilder, RGBUtility } from '../../src';
+
+describe('RGBBuilder', (): void => {
+    describe('setRed', (): void => {
+        test('setRed should return the builder object', (): void => {
+           const builder = new RGBBuilder();
+           expect(builder.setRed(5)).toEqual(builder);
+        });
+    });
+
+    describe('build', (): void => {
+        test('build should return an RGB object with default values', (): void => {
+            const builder = new RGBBuilder();
+            const rgb: RGB = builder.build();
+            expect(RGBUtility.isRGB(rgb)).toBeTruthy();
+            expect(rgb.red).toBe(0);
+            expect(rgb.green).toBe(0);
+            expect(rgb.blue).toBe(0);
+            expect(rgb.alpha).toBeUndefined();
+        });
+
+        test('build should return an RGB object with set RGB values', (): void => {
+            const red: 50 = 50 as const;
+            const green: 100 = 100 as const;
+            const blue: 150 = 150 as const;
+            const builder = new RGBBuilder();
+            const rgb: RGB = builder.setRed(red).setGreen(green).setBlue(blue).build();
+            expect(RGBUtility.isRGB(rgb)).toBeTruthy();
+            expect(rgb.red).toBe(red);
+            expect(rgb.green).toBe(green);
+            expect(rgb.blue).toBe(blue);
+            expect(rgb.alpha).toBeUndefined();
+        });
+
+        test('build should return an RGBA object with set RGBA values', (): void => {
+            const red: 200 = 200 as const;
+            const green: 150 = 150 as const;
+            const blue: 100 = 100 as const;
+            const alpha: 50 = 50 as const;
+            const builder = new RGBBuilder();
+            const rgb: RGB = builder.setRed(red).setGreen(green).setBlue(blue).setAlpha(alpha).build();
+            expect(RGBUtility.isRGB(rgb)).toBeTruthy();
+            expect(rgb.red).toBe(red);
+            expect(rgb.green).toBe(green);
+            expect(rgb.blue).toBe(blue);
+            expect(rgb.alpha).toBe(alpha);
+        });
+    });
+});
